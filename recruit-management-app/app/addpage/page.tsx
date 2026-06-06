3317daf4e3df4d849dde10df406ad22a2c7f2563
tanikiti9 <shelly.ai800@gmail.com>
2026-06-07 04:05:38 +0900

    ヘッダーの設定

--- recruit-management-app/app/addpage/page.tsx
+++ recruit-management-app/app/addpage/page.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React from 'react'
 
 const page = () => {
-  return <div>addpage</div>;
-};
+  return (
+    <div>page</div>
+  )
+}
 
-export default page;
+export default page
--- recruit-management-app/components/Header/Header.tsx
+++ recruit-management-app/components/Header/Header.tsx
@@ -1,7 +1,20 @@
+import Link from "next/link";
 import React from "react";
 
+
 const Header = () => {
-  return <div className="header">Header</div>;
+  return (
+    <header className="header">
+      <div className="header-title">
+        <Link href="/">就活管理</Link>
+      </div>
+      <Link href="/addpage">
+        <button className="add-btn">
+          新規追加
+        </button>
+      </Link>
+    </header>
+  );
 };
 
 export default Header;
