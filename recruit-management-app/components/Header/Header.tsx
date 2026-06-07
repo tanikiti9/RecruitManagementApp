c986dc88ed225efc237982d76814141bc61081af
tanikiti9 <shelly.ai800@gmail.com>
2026-06-07 13:28:44 +0900

    サンプルファイルの追加

--- recruit-management-app/components/Header/Header.tsx
+++ recruit-management-app/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="header">
+    <header className="header flex">
       <div className="header-title">
         <Link href="/">就活管理</Link>
       </div>
